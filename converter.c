171d9896d0c0ce9f55757f01ca030aeedc3029e3
Francis Enyeobi <fc.enyeobi@unizik.edu.ng>
2022-11-23 15:50:08 -0800

    converter.c updated

--- converter.c
+++ converter.c
@@ -9,19 +9,19 @@
  */
 char *convert(unsigned long int num, int base, int lowercase)
 {
-	static char *rcp;
+	static char *rep;
 	static char buffer[50];
 	char *ptr;
 
 	rep = (lowercase)
 		? "0123456789abcdef"
 		: "0123456789ABCDEF";
-		ptr = &buffer[49];
-		*ptr = '\0';
-		do {
-			*--ptr = rep[num % base];
-			num /= base;
-		} while (num != 0);
-		return (ptr);
-}
+	ptr = &buffer[49];
+	*ptr = '\0';
+	do {
+		*--ptr = rep[num % base];
+		num /= base;
+	} while (num != 0);
 
+	return (ptr);
+}
